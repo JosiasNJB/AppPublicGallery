5cfb4f40c9977a3aac43e9315b2cd2ed20323be6
Josias Neves <jojarbor3@gmail.com>
2022-09-23 09:08:19 -0300

    Initial commit

---
+++ app/src/main/java/neves/josias/apppublicgallery/MainActivity.java
@@ -0,0 +1,14 @@
+package neves.josias.apppublicgallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
